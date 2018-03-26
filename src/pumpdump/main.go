1f90437f81382cae579289462e64a5dabf4469b3
Anh Pha <phavo@minhhungland.vn>
2018-03-26 15:21:42 +0700

    get exchange info before do fomo

--- src/pumpdump/main.go
+++ src/pumpdump/main.go
@@ -27,8 +27,13 @@ func main() {
 
 	errs := make(chan error, 2)
 
-	worker := exchange.NewBinance(apiKey, secretKey)
-	worker.Fomo(pair, *total, *maxPrice, *tk, *sl, *delay, errs)
+	binance := exchange.NewBinance(apiKey, secretKey)
+	_, err := binance.GetExchangeInfo()
+	if err != nil {
+		errs <- fmt.Errorf("Cannot get exchange info: %s", err)
+	} else {
+		binance.Fomo(pair, *total, *maxPrice, *tk, *sl, *delay, errs)
+	}
 	go func() {
 		c := make(chan os.Signal)
 		signal.Notify(c, syscall.SIGINT)
